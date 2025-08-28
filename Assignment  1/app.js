dabccf11aa48ee60cc8a9df97b2181954a31a14c
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-08-28 01:48:30 -0900

    JS First Assignment

---
+++ Assignment  1/app.js
@@ -0,0 +1,61 @@
+
+//    First Chapter Alerts !
+
+// -------  question no 1  -----------
+
+//  alert ("Well Come to my Website");
+
+// -------  question no 2  -----------
+
+//   alert ("Error! Please enter a valid password");
+
+//  -------  question no 3  -----------
+
+//  alert ("Welcome to JS land...\nHappy Coding!" )
+
+//  -------  question no 4  -----------
+
+//  alert ("Welcome to JS land...");
+//  alert ("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+//  -------  question no 5  -----------
+
+//  alert ("Hello I can run JS through my browser's console");
+
+//    Sec Chapter Variables for String !
+
+//  -------  question no 6  -----------
+
+//  var userName ;
+
+//  -------  question no 7  -----------
+
+//  var myName =  "Malik Siddiq Awan";
+
+//  -------  question no 8  -----------
+
+// var message = "Hello World!";
+// alert (message);
+
+//  -------  question no 9  -----------
+
+//  var studentName = "Mahboob Ahmed";
+//  var age = "15 years old";
+//  var qualification = "Certified Mobile App Devlopment";
+
+//  alert (studentName);
+//  alert (age);
+//  alert (qualification);
+
+//  -------  question no 10  -----------
+
+//  var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//  alert (food);
+
+//  -------  question no 11  -----------
+
+
+
+
+
+
