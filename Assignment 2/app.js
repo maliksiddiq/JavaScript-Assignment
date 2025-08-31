c7077c0260b1189f5dbf790ef784dff29304f4f2
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-08-30 23:56:14 -0900

    Math Experision are Start

---
+++ Assignment 2/app.js
@@ -0,0 +1,10 @@
+
+//  ----------   MATH EXPRESSIONS ------------- 
+
+//  question no 1
+
+// var num1 =  3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.write ("Sum of"+" " + num1 + " and " + num2 + " " + "is" + " " + sum); 
+
