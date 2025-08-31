dbc738508f513cfb8be0b7ea8a35d91fa4fc3160
maliksiddiq <maliksiddiqawan21@gmail.com>
2025-08-31 03:19:18 -0900

    Math Experision Continued

--- Assignment 2/app.js
+++ Assignment 2/app.js
@@ -3,8 +3,137 @@
 
 //  question no 1
 
-// var num1 =  3;
-// var num2 = 5;
-// var sum = num1 + num2;
-// document.write ("Sum of"+" " + num1 + " and " + num2 + " " + "is" + " " + sum); 
+//   Addition
+
+//  var num1 =  3;
+//  var num2 = 5;
+//  var sum = num1 + num2;
+//  document.write ("Sum of"+" " + num1 + " and " + num2 + " " + "is" + " " + sum); 
+
+//  question no 2
+
+// ------------- Subtraction ------ !
+
+//  var num1 = 8;
+//  var num2 = 4;
+//  var subtract = num1 - num2;
+//  document.write ("if you subtract" + num1 + " by " + num2 + " you get " + subtract);
+
+// ------------- Multipication ------ !
+
+//  var num1 = 5;
+//  var num2 = 3;
+//  var multipication = num1*num2;
+//  document.write ("if you multiply " + num1 + " by " + num2 + ' you get ' + multipication);
+
+
+// ------------- Divison ------ !
+
+// var num1 = 9;
+// var num2 = 3;
+// var divison = num1 / num2;
+// document.write ("if you divide " + num1 + " by " + num2 + " you get " + divison);
+
+// ------------- Modulus ------ !
+
+//  var num1 = 11;
+//  var num2 = 3;
+//  var reminder = num1 % num2;
+//  document.write ("if you divide " + num1 + " by " + num2 + " you get the reminder " + reminder);
+
+//  question no 3
+
+//  var number;
+//  document.write ("Value after variable declaration  is " + number + "</br>");
+
+//  number = 5;
+//  document.write ("Initial value is:" + number + "</br>");
+
+//  var firstIncrement = ++number;
+//  document.write ("Value After increment is: " + firstIncrement + "</br>");
+
+//  var addition = number + 7;
+//  document.write ("value after addition is: " + addition + "</br>" );
+
+//  var decrement = --addition;
+//  document.write ("Value after decrement is: " + decrement + "</br>");
+
+//  var reminder = decrement % 3;
+//  document.write ("The reminder is: " + reminder);
+
+
+//  question no 4
+
+//  var ticketPrice = 600; 
+//  var total = ticketPrice*5;
+//  document.write ("Total cost to buy 5 tickets to a movie is " + total);
+
+//  question no 5
+
+// document.write ("<h3>Table of any number</h3>");
+
+// var a = +prompt ("Enter your number");
+// var b = 1;
+
+// document.write (a + " x " + b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+// document.write (a + " x " + ++b + " = " + a*b + "</br>") 
+
+
+//  question no 6
+
+// --------------------------------------------------------------------- ?
+
+
+//  question no 7
+
+// document.write ("<h1>Shopping Cart</h1>");
+
+// var priceItem1 = 650;
+// var priceItem2 = 100;
+// var quantityItem1 = 3;
+// var quantityItem2 = 7;
+// var charges = 100;
+
+// document.write ("Price of item 1 is " + priceItem1 + "</br>");
+// document.write ("Quantity of item 1 is " + quantityItem1 + "</br>");
+// document.write ("Price of item 2 is " + priceItem2 + "</br>");
+// document.write ("Quantity of item 2 is " + quantityItem2 + "</br>");
+// document.write ("Shipping charges is " + charges + "</br></br>");
+
+// var total = priceItem1*3 + priceItem2*7 + charges ;
+// document.write ("Total cost of your order is "+ total );
+
+
+//  question no 8 
+
+// document.write ("<h1>Mark Sheet </h1></br></br>");
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+
+// var percentage = (marksObtained / totalMarks) *100;
+// document.write ("Total marks: " + totalMarks +"</br>")
+// document.write ("Obtained marks:" + marksObtained +"</br>")
+// document.write ("Percentage:" + percentage + " % ");
+
+
+//  question no 8 
+
+// document.write ("<h1>Currency in PKR</h1>")
+
+// var usDollar = 104.80;
+// var saudiRiyal = 28;
+
+// var totalPkr = (usDollar*10) + (saudiRiyal*25);
+// document.write ("Total currency in PKR: "+ totalPkr);
+
+
 
